6250d8a8e924d7f15668c76770c143286f76bdb4
Abdulquadri-Mahmud <114073342+Abdulquadri-Mahmud@users.noreply.github.com>
2023-11-14 13:30:45 +0100

    Server and database created successfully

--- api/index.js
+++ api/index.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userAuthRoute from './routes/user.auth.routes.js'
+
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
+const port = 3000;
+
 mongoose.connect(process.env.db).then(() => {
-    console.log('Database Connected Successfully!');
-    app.listen(4000, () => {
-        console.log('Server is running on port 4000');
-    });
+    console.log('Database connected successfully');
 }).catch((error) => {
-    console.log(error, 'Error while connecting to server!');
+    console.log('Error while connecting to database');
 });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.use('/api/auth', userAuthRoute);
